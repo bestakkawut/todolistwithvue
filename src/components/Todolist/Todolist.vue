49258fc8a4bd86fe4cf5a39a8b8c9e85fc7beb15
Akkawut <akkawut@readyplanet.com>
2018-11-15 18:01:27 +0700

    add button to list todo

--- src/components/Todolist/Todolist.vue
+++ src/components/Todolist/Todolist.vue
@@ -1,11 +1,18 @@
 <template>
     <ul>
         <li v-for="(todo,index) in todos" :key="index" class="column has-text-left">
-            <h1 class="title is-size-4">
-                <input type="checkbox" @change="toggleComplete(index)">
-                 {{todo.title}}
-            </h1>
-            <p class="subtitle">{{todo.msg}}</p>
+            <div class="columns">
+                <div class="column">
+                    <h1 class="title is-size-4">
+                        <input type="checkbox" @change="toggleComplete(index)">
+                        {{todo.title}}
+                    </h1>
+                    <p class="subtitle">{{todo.msg}}</p>
+                </div>
+                <div class="column has-text-right">
+                    <button class="button">X</button>
+                </div>
+            </div>
         </li>
     </ul>
 </template>
